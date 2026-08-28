8bf5e333c0c0ec12a68ce9bc321169d6bb9df0c4
James DeVita <jnt39126-coder@users.noreply.github.com>
2026-08-28 18:28:18 -0400

    Include Plaid data in budget dashboard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="my-budget-v6";
+const C="my-budget-v7";
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.json"])).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
